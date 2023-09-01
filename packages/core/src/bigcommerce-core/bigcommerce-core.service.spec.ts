c3ce7ca7f194e6c1261f3b1da5f629a8a75c0b4c
edgeee <rasheedmusa9@gmail.com>
2023-09-01 17:21:27 +0200

    Configure Eslint

--- packages/core/src/bigcommerce-core/bigcommerce-core.service.spec.ts
+++ packages/core/src/bigcommerce-core/bigcommerce-core.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BigcommerceCoreService } from './bigcommerce-core.service';
+import { BIGCOMMERCE_CORE_OPTIONS } from './bigcommerce-core.constants';
 
 describe('BigcommerceCoreService', () => {
   let service: BigcommerceCoreService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BigcommerceCoreService],
+      providers: [
+        BigcommerceCoreService,
+        {
+          provide: BIGCOMMERCE_CORE_OPTIONS,
+          useValue: { clientId: 'client-id', clientSecret: 'client-secret' },
+        },
+      ],
     }).compile();
 
     service = module.get<BigcommerceCoreService>(BigcommerceCoreService);
--- packages/core/src/bigcommerce-core/bigcommerce-core.service.ts
+++ packages/core/src/bigcommerce-core/bigcommerce-core.service.ts
@@ -1,25 +1,28 @@
 import { Injectable, Inject } from '@nestjs/common';
-import {BIGCOMMERCE_CORE_OPTIONS} from "./bigcommerce-core.constants";
-import {BigCommerceCoreOptions} from "./bigcommerce-core.interface";
+import { BIGCOMMERCE_CORE_OPTIONS } from './bigcommerce-core.constants';
+import { BigCommerceCoreOptions } from './bigcommerce-core.interface';
 import * as BigCommerce from '@bigcommerce/api-nodejs';
 
 @Injectable()
 export class BigcommerceCoreService {
-  constructor(@Inject(BIGCOMMERCE_CORE_OPTIONS) private readonly clientOptions: BigCommerceCoreOptions) {}
+  constructor(
+    @Inject(BIGCOMMERCE_CORE_OPTIONS)
+    private readonly clientOptions: BigCommerceCoreOptions,
+  ) {}
 
   getRestClient(accessToken: string, storeHash: string) {
     return new BigCommerce.Rest({
-        storeHash: storeHash,
-        accessToken: accessToken,
-    })
+      storeHash: storeHash,
+      accessToken: accessToken,
+    });
   }
 
   getAuthClient(authCallback: string, loginHost?: string) {
     return new BigCommerce.Auth({
       clientId: this.clientOptions.clientId,
       clientSecret: this.clientOptions.clientSecret,
-      authCallback:  authCallback,
-      loginHost: loginHost
-    })
+      authCallback: authCallback,
+      loginHost: loginHost,
+    });
   }
 }
